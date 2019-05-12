e232330cb424e3d87ac83933451983e95a218c7d
Wanchao Zhang <wonderbeyond@gmail.com>
2019-05-12 22:42:06 +0800

    Updates

---
+++ precache-manifest.59683c78f7151476cfe2a67ba629dd6f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740ad3b0f64837e08c7a2b2c332d0973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea4d8e9d5ced90a805c",
+    "url": "/static/css/2.e97928ec.chunk.css"
+  },
+  {
+    "revision": "38192bdc6c4b1640cf0b",
+    "url": "/static/css/main.fed30062.chunk.css"
+  },
+  {
+    "revision": "fea4d8e9d5ced90a805c",
+    "url": "/static/js/2.01b56305.chunk.js"
+  },
+  {
+    "revision": "38192bdc6c4b1640cf0b",
+    "url": "/static/js/main.1ff3cd3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
