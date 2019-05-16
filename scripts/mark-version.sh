aeab66e1c2e7d01892ee49961850fd025c5eab26
Wanchao Zhang <wonderbeyond@gmail.com>
2019-05-16 22:35:02 +0800

    add custom google analytics dimension

--- scripts/mark-version.sh
+++ scripts/mark-version.sh
@@ -5,5 +5,6 @@ version=$1
 sed -i -re "s/REACT_APP_VERSION=.+/REACT_APP_VERSION=${version}/" .env
 sed -i -re "s/\"version\": \".+\"/\"version\": \"${version}\"/" package.json
 sed -i -re "s/\"version\": \".+\"/\"version\": \"${version}\"/" public/manifest.json
+sed -i -re "s/var appVer = \".+\"/var appVer = \"${version}\"/" public/ga.js
 
 echo "After you commit changes, don't forget: git tag ${version}"
