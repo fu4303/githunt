a9d00ee7b88b7f3afed4b7e960d4223005295386
Wanchao Zhang <wonderbeyond@gmail.com>
2019-05-26 17:11:16 +0800

    prevent unnecessary img loading in <BuiltByMembers/>

--- src/components/built-by-members/index.js
+++ src/components/built-by-members/index.js
@@ -8,6 +8,7 @@ import './styles.scss';
 
 class BuiltByMembers extends React.Component {
   state = {
+    readyForTooltipImgLoading: false
   };
 
   componentDidCatch(error, info) {
@@ -17,25 +18,33 @@ class BuiltByMembers extends React.Component {
   }
 
   render() {
-    return this.props.members.map(m => {
-      let targetID = `tooltip:${this.props.repository.author}/${this.props.repository.name}@${m.username}`;
-      return (
-        <span key={m.username} className="built-by-member-item">
-          <a href={`https://github.com/${m.username}`} data-tip data-for={targetID}
-            className="d-inline-block built-by-member-avatar" rel="noopener noreferrer" target="_blank">
-            <img alt={m.username} className="mini" src={`https://avatars.githubusercontent.com/${m.username}?s=40&v=4`} />
-          </a>
-          <ReactTooltip className="built-by-member-popup"
-            delayHide={200}
-            delayShow={200}
-            delayUpdate={200}
-            id={targetID} place="top" type="dark" effect="solid">
-            <img alt={m.username} src={`https://avatars.githubusercontent.com/${m.username}?s=200&v=4`} />
-            <span className="username">{m.username}</span>
-          </ReactTooltip>
-        </span>
-      )
-    })
+    return <span style={{ display: 'inline-block' }} onMouseEnter={() => this.setState({ readyForTooltipImgLoading: true })}>
+      {this.props.members.map(m => {
+        let targetID = `tooltip:${this.props.repository.author}/${this.props.repository.name}@${m.username}`;
+        return (
+          <span key={m.username} className="built-by-member-item">
+            <a href={`https://github.com/${m.username}`} data-tip="" data-for={targetID}
+              className="d-inline-block built-by-member-avatar" rel="noopener noreferrer" target="_blank">
+              <img alt={m.username} className="mini" src={`https://avatars.githubusercontent.com/${m.username}?s=40&v=4`} />
+            </a>
+            <ReactTooltip className="built-by-member-popup"
+              id={targetID} place="top" type="dark" effect="solid"
+              delayHide={200}
+              delayShow={200}
+              delayUpdate={200}
+              getContent={(T) => {
+                console.log(`Getting content of ${m.username}`, this);
+                if (!this.state.readyForTooltipImgLoading) { return null }
+                return <>
+                  <img alt={m.username} src={`https://avatars.githubusercontent.com/${m.username}?s=200&v=4`} />
+                  <span className="username">{m.username}</span>
+                </>
+              }}>
+            </ReactTooltip>
+          </span>
+        )
+      })}
+    </span>
   }
 }
 
