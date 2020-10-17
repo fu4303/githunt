701564a3a31a7050b3afa3ff5d9c7e76c6ac7345
yichen <tomato.stao@gmail.com>
2020-10-17 17:12:54 +0800

    :ambulance: Fix trending api url and eslint

--- public/ga.js
+++ public/ga.js
@@ -10,5 +10,5 @@ ga('create', 'UA-134825122-1', 'auto');
 ga('set', 'checkProtocolTask', function(){}); // // Removes failing protocol check.
 // ga('send', 'pageview'); // we send pv in react-router hook now
 
-var appVer = "4.11.2";
+var appVer = "4.11.3";
 ga('set', 'dimension1', appVer);
